678858729d1ed5a56c4b96df7e04a9cdb777a44e
CursoEGG001 <soygecko@gmail.com>
2023-06-15 21:55:47 -0300

    Simple aplicacion de usar 2 numeros

---
+++ Ejercicio03/src/Entidad/DivisionNumero.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+/**
+ *
+ * @author pc
+ */
+public class DivisionNumero {
+    
+    public int numA;
+    public int numB;
+
+    public DivisionNumero() {
+    }
+
+    public static int hacer(int numA, int numB) {
+        return numA / numB;
+    }
+}
---
+++ Ejercicio03/src/clase13/Ejercicio03.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase13;
+
+import Entidad.DivisionNumero;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class Ejercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in,"UTF-8").useDelimiter("\n");
+        
+        int primerNum;
+        int segundoNum;
+        
+        System.out.println("Ingrese 2 numeros :");
+        primerNum=leer.nextInt();
+        segundoNum=leer.nextInt();
+        System.out.println(DivisionNumero.hacer(primerNum, segundoNum));
+    }
+
+}
