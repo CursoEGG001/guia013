3622a8229e12e63eaed2217e7ba8a05589f422ff
CursoEGG001 <soygecko@gmail.com>
2023-06-17 13:12:49 -0300

    Eror a las UNA! erro a las DOS! Error a las...

---
+++ Ejercicio08/src/clase13/Ejercicio08.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase13;
+
+import Entidad.Dos;
+import Entidad.Tres;
+import Entidad.Uno;
+
+/**
+ *
+ * @author pc
+ */
+public class Ejercicio08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Para Uno...");
+        Uno.main(args);
+        System.out.println("Para Dos...");
+        Dos.main(args);
+        System.out.println("Para Tres...");
+        Tres.main(args);
+    }
+    
+}
